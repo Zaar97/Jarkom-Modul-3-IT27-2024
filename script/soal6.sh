d12c122afda784cb140023d589a13432da7b52df
Zaar97 <128958228+Zaar97@users.noreply.github.com>
2024-05-19 12:02:46 +0700

    Update soal6.sh

--- script/soal6.sh
+++ script/soal6.sh
@@ -1,33 +1,42 @@
-wget --no-check-certificate 'https://drive.google.com/file/d/1lmnXJUbyx1JDt2OA5z_1dEowxozfkn30/view?usp=sharing' -O '/var/www/harkonen.it27.com'
-unzip -o /var/www/harkonen.it27.com -d /var/www/
-rm /var/www/harkonen.it27.com
-mv /var/www/modul-3 /var/www/harkonen.it27.com
+apt-get update
+apt-get install nginx wget unzip php7.3 php-fpm -y
 
-cp /etc/nginx/sites-available/default /etc/nginx/sites-available/harkonen.it27.com
-ln -s /etc/nginx/sites-available/harkonen.it27.com /etc/nginx/sites-enabled/
-rm /etc/nginx/sites-enabled/default
+service nginx start
 
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1lmnXJUbyx1JDt2OA5z_1dEowxozfkn30' -O 'harkonen.zip'
+unzip harkonen.zip
+
+# meletakkan komponen web
+mkdir -p /var/www/harkonen.it27.com
+mv /root/modul-3/* /var/www/harkonen.it27.com/
+
+rm -rf /root/modul-3
+rm harkonen.zip
 
 echo 'server {
     listen 80;
-    server_name _;
 
+    server_name _;
 
     root /var/www/harkonen.it27.com;
     index index.php index.html index.htm;
-
-
+    
     location / {
         try_files $uri $uri/ /index.php?$query_string;
     }
 
-
     location ~ \.php$ {
         include snippets/fastcgi-php.conf;
-        fastcgi_pass unix:/run/php/php7.3-fpm.sock;  # Sesuaikan versi PHP dan socket
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        include fastcgi_params;
+        fastcgi_pass unix:/var/run/php/php7.3-fpm.sock;
     }
-}' > /etc/nginx/sites-available/harkonen.it27.com
+
+    error_log /var/log/nginx/harkonen.it27.com_error.log;
+    access_log /var/log/nginx/harkonen.it27.com_access.log;
+}
+' > /etc/nginx/sites-available/harkonen.it27.com.conf
+
+rm /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/harkonen.it27.com.conf /etc/nginx/sites-enabled/
 
 service nginx restart
+service php7.3-fpm start
