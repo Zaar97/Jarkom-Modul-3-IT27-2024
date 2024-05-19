2963a8ef861c09ba562edef0afec50e182bbcaf5
Zahra Sekar <128958228+Zaar97@users.noreply.github.com>
2024-05-19 20:38:21 +0700

    Update soal10.sh

--- script/soal10.sh
+++ script/soal10.sh
@@ -20,12 +20,13 @@ server {
 
     server_name _;
 
-    location / {
-        proxy_pass http://worker;   
+        location / {
+
+        proxy_pass http://worker;
+
+        auth_basic "Restricted Content";
+        auth_basic_user_file /etc/nginx/rahasisakita/htpasswd;
     }
-    
-    auth_basic "Restricted Content";
-    auth_basic_user_file /etc/nginx/supersecret/htpasswd;
 }' > /etc/nginx/sites-available/lb_php
 
 ln -sf /etc/nginx/sites-available/lb_php /etc/nginx/sites-enabled/
